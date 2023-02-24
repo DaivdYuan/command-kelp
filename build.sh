1a5f6ecd7a3e2344020c829b9cd2841941ace34b
DaivdYuan <47169162+DaivdYuan@users.noreply.github.com>
2023-02-23 17:38:21 -0800

    added requirements.txt

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+pip3 install -r requirements.txt && \
 g++ -Wall -Wextra -Werror -pedantic -o kelp kelp.cpp && \
 mkdir -p /usr/local/lib/command_helper && \
 cp helper.py /usr/local/lib/command_helper/helper.py && \
